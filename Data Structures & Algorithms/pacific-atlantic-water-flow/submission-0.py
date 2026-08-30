e615f0a2d809059b017035985b35b3cff487576b
pulakhandam lalith sai kishan <saikishan2008@gmail.com>
2026-08-30 13:22:00 -0700

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        N = len(heights)
+        M = len(heights[0])
+
+        marker = [[0] * len(heights[0]) for _ in range(len(heights))]
+        result = set()
+        def dfs(x, y, value, level):
+            if x < 0 or y < 0 or x >= N or y >= M or marker[x][y] >= value or heights[x][y] < level:
+                return
+            
+            marker[x][y] += value
+
+            if marker[x][y] == 3:
+                result.add((x, y))
+            
+            for i, j in ((0, 1), (0, -1), (-1, 0), (1,0)):
+                dfs(x + i, y + j, value, heights[x][y])
+        
+        for i in range(0, N):
+            dfs(i, 0, 1, 0)
+        
+        for j in range(0, M):
+            dfs(0, j, 1, 0)
+        
+        for i in range(0, N):
+            dfs(i, M-1, 2, 0)
+
+        for j in range(0, M):
+            dfs(N-1, j, 2, 0)
+        
+        return list(result)
+        
+            
+
+
+            
+            
+
